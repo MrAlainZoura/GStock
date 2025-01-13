05d8df26198ca123d1cb6a3030c5b010671f3d76
MrAlainZoura <a.tshiyanze@gmail.com>
2025-01-13 19:55:31 +0100

    mode migration controller depot-produit

---
+++ database/migrations/2025_01_13_171208_create_produit_depots_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produit_depots', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('depot_id')
+                ->constrained()
+                ->noActionOnDelete()
+                ->cascadeOnUpdate();
+            $table->foreignId('produit_id')
+                ->constrained()
+                ->noActionOnDelete()
+                ->cascadeOnUpdate();
+            $table->integer('quantite');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produit_depots');
+    }
+};
