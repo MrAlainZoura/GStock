e5633d611b1b7b98c70c87290a0f836cbcdeeb30
MrAlainZoura <a.tshiyanze@gmail.com>
2025-02-03 10:32:04 +0100

    vue transfert index, tada table et initialisation vue transfert show vue

---
+++ resources/views/composant/dataTableTrans.blade.php
@@ -0,0 +1,146 @@
+
+<table id="search-table" class="w-70">
+    <thead>
+        <tr>
+            <th>
+                <span class="flex items-center max-w-sm">
+                    code
+                    <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                    </svg>
+                </span>
+            </th>
+            <th>
+                <span class="flex items-center max-w-sm">
+                    Initiateur
+                    <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                    </svg>
+                </span>
+            </th>
+            <th>
+                <span class="flex items-center max-w-sm">
+                    Reception
+                    <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                    </svg>
+                </span>
+            </th>
+            <th>
+                <span class="flex items-center max-w-sm">
+                    Quantité
+                    <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                    </svg>
+                </span>
+            </th>
+            <th>
+                <span class="flex items-center">
+                Destination
+                    <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                    </svg>
+                </span>
+            </th>
+            <th>
+                <span class="flex items-center">
+                Action
+                    <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                    </svg>
+                </span>
+            </th>
+          
+        </tr>
+    </thead>
+    <tbody>
+
+        @foreach ($data as $key=>$item )
+        <tr>
+            <td class="font-medium text-gray-900  dark:text-white">
+                <div class="flex gap-2 sm:bloc">
+                    {{$item->code}}
+                </div>
+            </td>
+            <td >
+                {{$item->created_at}}<br/>
+                {{$item->user->name}}   
+            </td>
+            <td >
+                @if ($item->receptionUser==null)
+                <span class="inline-flex items-center justify-center w-6 h-6 me-2 text-sm font-semibold text-blue-800 bg-blue-100 rounded-full dark:bg-gray-700 dark:text-blue-400">
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path fill="#FF0000" d="m18.774 8.245-.892-.893a1.5 1.5 0 0 1-.437-1.052V5.036a2.484 2.484 0 0 0-2.48-2.48H13.7a1.5 1.5 0 0 1-1.052-.438l-.893-.892a2.484 2.484 0 0 0-3.51 0l-.893.892a1.5 1.5 0 0 1-1.052.437H5.036a2.484 2.484 0 0 0-2.48 2.481V6.3a1.5 1.5 0 0 1-.438 1.052l-.892.893a2.484 2.484 0 0 0 0 3.51l.892.893a1.5 1.5 0 0 1 .437 1.052v1.264a2.484 2.484 0 0 0 2.481 2.481H6.3a1.5 1.5 0 0 1 1.052.437l.893.892a2.484 2.484 0 0 0 3.51 0l.893-.892a1.5 1.5 0 0 1 1.052-.437h1.264a2.484 2.484 0 0 0 2.481-2.48V13.7a1.5 1.5 0 0 1 .437-1.052l.892-.893a2.484 2.484 0 0 0 0-3.51Z"/>
+                <path fill="#fff" d="M8 13a1 1 0 0 1-.707-.293l-2-2a1 1 0 1 1 1.414-1.414l1.42 1.42 5.318-3.545a1 1 0 0 1 1.11 1.664l-6 4A1 1 0 0 1 8 13Z"/>
+                </svg>
+                <span class="sr-only">Non confirmé</span>
+                </span>
+                
+                Non confirmé
+                @else
+                <span class="inline-flex items-center justify-center w-6 h-6 me-2 text-sm font-semibold text-blue-800 bg-blue-100 rounded-full dark:bg-gray-700 dark:text-blue-400">
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path fill="#00FF00" d="m18.774 8.245-.892-.893a1.5 1.5 0 0 1-.437-1.052V5.036a2.484 2.484 0 0 0-2.48-2.48H13.7a1.5 1.5 0 0 1-1.052-.438l-.893-.892a2.484 2.484 0 0 0-3.51 0l-.893.892a1.5 1.5 0 0 1-1.052.437H5.036a2.484 2.484 0 0 0-2.48 2.481V6.3a1.5 1.5 0 0 1-.438 1.052l-.892.893a2.484 2.484 0 0 0 0 3.51l.892.893a1.5 1.5 0 0 1 .437 1.052v1.264a2.484 2.484 0 0 0 2.481 2.481H6.3a1.5 1.5 0 0 1 1.052.437l.893.892a2.484 2.484 0 0 0 3.51 0l.893-.892a1.5 1.5 0 0 1 1.052-.437h1.264a2.484 2.484 0 0 0 2.481-2.48V13.7a1.5 1.5 0 0 1 .437-1.052l.892-.893a2.484 2.484 0 0 0 0-3.51Z"/>
+                <path fill="#fff" d="M8 13a1 1 0 0 1-.707-.293l-2-2a1 1 0 1 1 1.414-1.414l1.42 1.42 5.318-3.545a1 1 0 0 1 1.11 1.664l-6 4A1 1 0 0 1 8 13Z"/>
+                </svg>
+                <span class="sr-only">Confirmé</span>
+                </span>
+                {{$item->receptionUser}}
+                <br/>
+                {{$item->updated_at}}
+                @endif
+            </td>
+            <td>
+                @php
+                    $totalQt = 0;
+                    foreach($item->produitTransfert as $val){
+                        $totalQt+=$val->quantite;
+                    }
+                @endphp
+                {{$totalQt}}
+                @if($totalQt >=2 )
+                pièces
+                @else
+                pièce
+                @endif
+            </td>
+            <td > 
+                {{$item->destination}}            
+            </td>
+            <td>
+                @include('composant.actionLink', ['itemName'=>$item->code,'seeRoute'=>'transShow','seeParam'=>56745264509*$item->id."adft".$item->id*6789012345, 'deleteRoute'=>"transDelete",'deleteParam'=>56745264509*$item->id."adft".$item->id*6789012345, 'editeRoute'=>"transEdit",'editParam'=>$item->code." ".$item->id*6789012345])
+            </td>
+        </tr>
+        @endforeach
+
+    </tbody>
+</table>
+
+
+
+<script>
+    
+if (document.getElementById("search-table") && typeof simpleDatatables.DataTable !== 'undefined') {
+    const dataTable = new simpleDatatables.DataTable("#search-table", {
+        searchable: true,
+        paging: true,
+        perPage: 5,
+        perPageSelect: [5, 10, 15, 20, 25, 50,100, 200, 300, 400, 500],
+        sortable: true
+    });
+}
+
+ //modal suppression item
+    const deleteLink = document.querySelectorAll('#linkDelete');
+    deleteLink.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const hrefClicked = event.currentTarget.getAttribute('href');
+            const formDelete =document.getElementById('deleteForm');
+            const textDeleteItem =document.getElementById('textDeleteItem');
+            const itemName = event.currentTarget.getAttribute('itemName') ;
+            textDeleteItem.textContent= `Confirmer la suppression du transfert ${itemName}`;
+            formDelete.setAttribute('action',hrefClicked);
+        });
+    });
+</script>
--- resources/views/transfert/index.blade.php
+++ resources/views/transfert/index.blade.php
@@ -1,8 +1,8 @@
 @extends('base')
-@section('title', "Approvisionnement $depot->libele")
+@section('title', "Transfert {{session('depot')}}")
 
 @section('header')
-  @include('composant.hearder', ['user_email'=>"$user->email", 'user_name'=>"$user->name"])
+  @include('composant.hearder', ['user_email'=>Auth::user()->email, 'user_name'=>Auth::user()->name])
 @endsection
 
 @section('main')  
@@ -20,15 +20,17 @@
     
 
     <section class="p-10 gap-5 w-full">
-     
-        @include('composant.dataTableAppro', ['data'=>$depot->approvisionnement])
-       
+        <div class="py-4 px-2 mx-auto max-w-screen-xl text-left lg:py-4">
+            <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Liste de transfert de {{session('depot')}} </h1>
+        </div>
+        @include('composant.dataTableTrans',['data'=>$findTransDepot])
     </section>
     
 
-    @include('composant.sidebar',['depot'=> $depot->libele])
+    @include('composant.sidebar',['depot'=> session('depot')])
     @endsection
 
+    @include('composant.modalDelete')
 
 @section('footer')
     @include('composant.footer')
