4728827b91a1720ddb079cb0d02077b85dea7837
MrAlainZoura <a.tshiyanze@gmail.com>
2025-02-07 19:35:04 +0100

    enregistrement de vente fini premiere version

--- app/Http/Controllers/VenteController.php
+++ app/Http/Controllers/VenteController.php
@@ -6,10 +6,13 @@ use App\Models\Depot;
 use App\Models\Vente;
 use App\Models\Client;
 use App\Models\Categorie;
+use Carbon\CarbonInterface;
 use App\Models\ProduitDepot;
+use App\Models\VenteProduit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
-use Carbon\CarbonInterface;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class VenteController extends Controller
 {
@@ -38,8 +41,85 @@ class VenteController extends Controller
      */
     public function store(Request $request, $depot)
     {
-        dd($request->all());
-        return back()->with("success","bientot disponible");
+        // dd($request->all());
+        $validateDate = Validator::make($request->all(),
+        [
+            'nom_client'=>'string|required',
+            'lieu_de_vente'=>'string|required',
+            'contact_client'=>'required|max:255',
+            'produits'=>'array|required'
+        ]);
+        if($validateDate->fails()){
+            return back()->with('echec',$validateDate->errors());
+        }
+        $a =Auth::user()->name[0] ;
+        $b = (Auth::user()->postnom !=null)?Auth::user()->postnom[0]:null ;
+        $c =(Auth::user()->prenom !=null)? Auth::user()->prenom[0]:null;
+
+        $initialUser = "$a$b$c";
+        $numero = Vente::where("user_id", Auth::user()->id)->where("created_at",'like','%'.Carbon::now()->format('Y-m-d').'%')->count()+1;
+        $numero =($numero < 10)?"0$numero":$numero;
+        $dataClient = [
+            'name'=>$request->nom_client,
+            'prenom'=>$request->prenom,
+            'tel'=>$request->contact_client,
+            'adresse'=>$request->adresse,
+            "genre"=>$request->genre
+        ];
+        $filterDataClient = array_filter($dataClient, function($val){return !is_null($val);});
+        $tel = "0".substr($filterDataClient['tel'], 4);
+        $findClient = Client::where('tel',$filterDataClient['tel'])->orWhere('tel',)->get();
+        $client_id ="";
+        if(count($findClient) > 0){
+            $client_id =$findClient[0]->id;
+        }else{
+            $createClient = Client::create($filterDataClient);
+            $client_id = $createClient->id;
+        }
+        $tabDataVenteProduit = [];
+
+        foreach($request->produits as $key=>$val){
+            $verifQTe = ProduitDepot::where('produit_id',$key)->where('depot_id',$request->depot_id/98123)->first();
+            foreach($val as $k=>$v){
+
+                if($verifQTe->quantite >= $k){
+                    $dataSave['id']=$key;
+                    $dataSave['qt']=$k;
+                    $dataSave['pt']=$v;
+                    array_push($tabDataVenteProduit,$dataSave);
+                }
+            }
+        }
+        // dd($tabDataVenteProduit);
+        if($tabDataVenteProduit!=null){
+            $dataVente = [
+                'user_id'=>Auth::user()->id,
+                'depot_id'=>$request->depot_id/98123,
+                'code'=>"N°$numero/$initialUser",
+                'client_id'=>$client_id,
+                'type'=>$request->lieu_de_vente
+            ] ;
+
+            $createVente = Vente::create($dataVente);
+            if($createVente){
+                foreach($tabDataVenteProduit as $valeur){
+                    // dd( $valeur['id'], $valeur['qt'], $valeur['pt']);
+                    $dataVenteProduit =[
+                        'produit_id'=>$valeur['id'],
+                        'vente_id'=>$createVente->id,
+                        'quantite'=>$valeur['qt'],
+                        'prixU'=>(int)$valeur['pt']/(int)$valeur['qt'],
+                        'prixT'=>$valeur['pt']
+                    ];
+                    $createVenteProduit = VenteProduit::create($dataVenteProduit);
+                    if($createVenteProduit){
+                        return back()->with("success","Vente effectué avec succes, numéro $createVente->code par ".Auth::user()->name);
+                    }
+                }
+            }
+        }        
+        return back()->with("echec","Vente echouée, une erreur inattendue s'est produite.");
+
     }
 
     /**
--- resources/views/vente/create.blade.php
+++ resources/views/vente/create.blade.php
@@ -22,6 +22,7 @@
         <form id="myFormVente" action="{{route('venteStore',session('depot'))}}" method="post" enctype="multipart/form-data">
             @csrf
             @method('post')
+            <input type="hidden" name="depot_id" value="{{$depot->id*98123}}">
               <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                 <div>
                     <div class="sm:col-span-2 mb-2">
@@ -36,7 +37,7 @@
                     </div>
                         <div class="w-full">
                             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom</label>
-                            <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="nom" required="">
+                            <input type="text" name="nom_client" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="nom" required="">
                         </div>
                         <div class="w-full">
                             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prenom</label>
@@ -53,7 +54,7 @@
                        
                         <div class="w-full">
                             <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Téléphone</label>
-                            <input type="text" id="tel" name="tel" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="+243 ..." >
+                            <input type="text" id="tel" name="contact_client" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="+243 ..." >
                         </div>
                         <div class="w-full">
                             <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Adresse</label>
@@ -71,6 +72,13 @@
                             <hr class="h-px  bg-black border-0 dark:bg-gray-700">
                         </div>
                         <div class="grid gap-4 grid-cols-1 sm:gap-6">
+                            <div class="w-full sm:col-span-2">
+                                <label for="lieu" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Lieu de vente</label>
+                                <select id="lieu" name="lieu_de_vente" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                    <option selected value="Shop">Au shop</option>
+                                    <option value="Livraison">Livraison</option>
+                                </select>
+                            </div>
                             <div class="w-full sm:col-span-2" onclick="event.stopImmediatePropagation();">
                                 <input type="text" 
                                     id="autocompleteInput"
