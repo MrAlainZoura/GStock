65a0280b5dc029f17e7cb6a17cad3b01cdf39aa2
MrAlainZoura <a.tshiyanze@gmail.com>
2025-07-07 19:31:47 +0100

    build npm

---
+++ app/Models/Devise.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Devise extends Model
+{
+    protected $fillable = [
+        'depot_id',
+        'libele',
+        'taux'
+    ];
+
+    public function depot(){
+        return $this->belongsTo(Depot::class);
+    }
+}
