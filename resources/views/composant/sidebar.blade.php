72ce2114d3620ae1e06e625698bc43a6b2afed44
MrAlainZoura <a.tshiyanze@gmail.com>
2025-07-28 17:53:42 +0100

    update transfert create autocomplete sidebar butoun exit

--- resources/views/composant/sidebar.blade.php
+++ resources/views/composant/sidebar.blade.php
@@ -254,10 +254,15 @@
          @endif
          <li>
             <form action="{{route('logout')}}" method="post" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <img src="{{asset('svg/exit.svg')}}" class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" alt="">
-                @csrf 
-                @method('post')
-                <button type="submit" class="ml-3">Exit</button>
+               @csrf 
+               @method('post')
+               <button type="submit" class="hover:cursor-pointer flex items-center w-full">
+                  <img src="{{asset('svg/exit.svg')}}" 
+                        class="mr-2 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" 
+                        aria-hidden="true" 
+                        alt="Exit icon"/>
+                  Exit
+               </button>
             </form>
          </li>
       </ul>
