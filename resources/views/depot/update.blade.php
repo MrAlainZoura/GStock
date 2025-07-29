ffe6f807d55fd189c50739ff949808f7c63ebfa5
MrAlainZoura <a.tshiyanze@gmail.com>
2025-07-29 16:58:37 +0100

    update de dive dans le parametre

--- resources/views/depot/update.blade.php
+++ resources/views/depot/update.blade.php
@@ -143,7 +143,7 @@
                 <div class="sm:col-span-2">
                   <label for="region" class="block text-sm/6 font-medium text-gray-900">Etat / Province</label>
                   <div class="mt-2">
-                    <input type="text" name="ville" value="{{ $getDepotInformation->ville }}" id="region" autocomplete="address-level1" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                    <input type="text" name="province" value="{{ $getDepotInformation->province }}" id="region" autocomplete="address-level1" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                   </div>
                 </div>
 
