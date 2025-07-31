ec17817059180dc185812a103e15ad9a4b0189cd
MrAlainZoura <a.tshiyanze@gmail.com>
2025-07-31 09:31:18 +0100

    update user plusieurs depot

--- resources/views/composant/hearder.blade.php
+++ resources/views/composant/hearder.blade.php
@@ -25,7 +25,9 @@
         <ul class="py-2" aria-labelledby="user-menu-button ">
         
           <li>
-            <a  href="{{route('user.edit',Auth::user()->id*6789012345)}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profil</a>
+            @if(Route::current()->getName() !=='dashboard')
+              <a  href="{{route('user.edit',Auth::user()->id*6789012345)}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profil</a>
+            @endif
           </li>
           <li> 
             <form action="{{route('logout')}}" method="post">
