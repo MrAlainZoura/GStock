accfb068009038586fe6d86d8a2825064e84c282
MrAlainZoura <a.tshiyanze@gmail.com>
2025-08-27 17:29:49 +0100

    add compassassion dans rapport

--- resources/views/composant/rapportTable.blade.php
+++ resources/views/composant/rapportTable.blade.php
@@ -47,8 +47,8 @@
                 </td>
                 <td class="px-6 py-4">
                     @php
-                        $recette +=$v->prixT;
-                        $recetteFc += $v->prixT * $item->updateTaux;
+                        $recette +=(float)$v->prixT;
+                        $recetteFc += (float)$v->prixT * (float)$item->updateTaux;
                     @endphp
                 @formaMille($v->prixT) {{ $item->devise->libele }}
                 </td>
@@ -69,6 +69,64 @@
            
             @endforeach
             @endforeach
+            @if (count($compassassion) >0)
+            <tr>
+                <th scope="col" colspan="4" class="px-6 py-3">
+                    Contre valeur - compassassion
+                </th>
+            </tr>
+            @foreach ($compassassion as $k=>$v )
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{$v->code}}
+                    </th>
+                    <td class="py-2" colspan="2">
+                        Echangé(s)
+                        <ol class="ps-2 mt-2 space-y-1 list-decimal list-inside">
+                            @foreach ($v->compassassion as $comp)
+                                <li>
+                                    {{ $comp->produit->libele }} {{ $comp->quantite }} pc 
+                                    <span class="text-xl text-gray-600">→</span>
+                                    {{ $comp->prixT }} {{ $item->devise->libele }}</li>
+                            @endforeach
+                        </ol>
+                           <br> 
+                        Contre (ancien article)
+                        <ol class="ps-5 mt-2 space-y-1 list-decimal list-inside">
+                           @foreach ($v->venteProduit as $anV)
+                           <li>
+                                {{ $anV->produit->libele }} {{ $anV->quantite }}pc 
+                                <span class="text-xl text-gray-600">→</span>
+                                {{ $anV->prixT }} {{ $item->devise->libele }}
+                            </li>
+                           @endforeach 
+                        </ol>
+                    </td>
+                    <td class="px-5 py-4">
+                        {{ $v->paiement->sortBy('created_at')->first()->net}} + {{ $v->paiement->sortByDesc('created_at')->first()->avance }}<br>
+                        {{ $v->paiement->sortByDesc('created_at')->first()->net }} {{ $item->devise->libele }}
+                         @php
+                            $recette +=(float) $v->paiement->sortByDesc('created_at')->first()->avance;
+                            $recetteFc += (float)$v->paiement->sortByDesc('created_at')->first()->avance* (float)$v->updateTaux;
+                        @endphp
+                    </td>
+                    <td class="px-6 py-4">
+                        {{$v->updateTaux}}Fc
+                    </td>
+                    <td class="px-6 py-4">
+                        Shop
+                    </td>
+                    <td class="px-6 py-4">
+                    @if($v->user != null)
+                        {{$v->user->name}}
+                    @else
+                        Utilisateur à problème
+                    @endif
+                    </td>
+                </tr>
+               
+                @endforeach
+            @endif
             <tr>
                 
                 <th scope="row" class="px-6 py-4 text-xl uppercase text-gray-900 whitespace-nowrap dark:text-white">
