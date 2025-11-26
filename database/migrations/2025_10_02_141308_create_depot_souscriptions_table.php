ff720a17e81a5924079d1282163d9c9ca8be684e
mr zou <a.tshiyanze@gmail.com>
2025-11-26 07:37:44 +0100

    migrationn abonnement

---
+++ database/migrations/2025_10_02_141308_create_depot_souscriptions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('depot_souscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('depot_id')
+                    ->nullable()
+                    ->constrained()
+                    ->nullOnDelete();
+            $table->foreignId('souscription_id')
+                    ->nullable()
+                    ->constrained()
+                    ->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('depot_souscriptions');
+    }
+};
